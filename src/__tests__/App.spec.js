c848284dc0bbdf69c13f27eb624f15e952454f47
nasoavinaornel <nasoavinaornel@gmail.com>
2026-06-16 23:22:30 +0200

    ci: ajout du pipeline de test automatisé avec GitHub Actions

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,11 +1,10 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
 
-describe('App', () => {
-  it('mounts renders properly', () => {
+describe('App.vue', () => {
+  it("affiche correctement le nom de l'étudiant", () => {
     const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+    expect(wrapper.text()).toContain('Nasoavina Ornel')
   })
 })
